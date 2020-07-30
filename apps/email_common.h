47340307ba49f969dd74de9d327675c83b336ef7
Bobbi Webber-Manners <bmanners@krakenrobotics.com>
2020-07-29 22:51:31 -0400

    Bump up version to v0.91.

--- apps/email_common.h
+++ apps/email_common.h
@@ -4,7 +4,7 @@
 // Bobbi June 2020
 /////////////////////////////////////////////////////////////////
 
-#define PROGNAME "emai//er v0.90"
+#define PROGNAME "emai//er v0.91"
 
 // Configuration params from POP65.CFG
 char cfg_server[40];         // IP of POP3 server
