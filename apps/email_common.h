077bddc9f1a9c6c68ee0d0129a4dda2e7eef339c
Bobbi Webber-Manners <bobbi.manners@gmail.com>
2021-11-16 15:03:32 -0500

    Bump upversion to 2.1.13.

--- apps/email_common.h
+++ apps/email_common.h
@@ -6,7 +6,7 @@
 
 #include <stdint.h>
 
-#define PROGNAME "emai//er v2.1.12"
+#define PROGNAME "emai//er v2.1.13"
 
 // Configuration params from EMAIL.CFG
 char cfg_server[40];         // IP of POP3 server
